f5b4b2b0c2cbaa7022d5f6983ed9503f24e8ef0e
vagnerasilva <vagnerasilva>
2021-10-23 21:04:33 -0300

    atualizando imagens

--- build.sh
+++ build.sh
@@ -1,16 +1,39 @@
 #!/bin/bash
-# declare STRING variable
-STRING="Hello World"
-#print variable on a screen
-echo $STRING
+REGISTRY="vagnerasilva"
+VERSION="latest"
+
 
 for i in $(ls -d */); 
-    do echo ${i}
-    #docker build -t ${i} -f ${i}/Dockerfile . ;
+    do 
+    #echo ${i:0:-1} # Removendo "/" da ultima posicao da string
+    echo "## START ##"
+    echo " "
+    
+    VALUE2="${i:0:-1}/Dockerfile ."   # criando nome da imagem a ser buildada
+    VALUE1="container-${i:0:-1}"      # criando nome do container que sera utilizado  
+    
+    echo ${VALUE2}
+    echo ${VALUE1}
+    docker build -t ${VALUE1} -f ${VALUE2}   # Realizando build dos containers com o nome das pastas e dockerfiles em cada uma delas 
+    #echo ${VALUE1} ${REGISTRY}/${VALUE1}:${VERSION}
+    echo " "
+    echo "## Build Done ##"
+    docker tag ${VALUE1} ${REGISTRY}/${VALUE1}:${VERSION} # colocando tag para subir no dockerhub
+    echo " "
+    echo "## Tag Done ##"
+    sleep 1
+    docker push ${REGISTRY}/${VALUE1}:${VERSION}         # subindo no dockerhub   
+    #docker images
+    echo " "
+    echo " "
+    echo "## Docker push Done ##"
+    echo " "
+    echo " "
+    sleep 1;
 done
-docker build -t img1 -f img1/Dockerfile .
-docker build -t img2 -f img2/Dockerfile .
-docker build -t img3 -f img3/Dockerfile .
 
 echo 'Done'
 docker images
+
+
+
--- readme.md
+++ readme.md
@@ -18,3 +18,28 @@ docker build -t img1 -f img1/Dockerfile .
 docker build -t img2 -f img2/Dockerfile .
 docker build -t img3 -f img3/Dockerfile .
 
+docker build -t julia-container .
+
+
+for i in $(ls -d */); 
+    do echo ${i}
+    VALUE2="${i}/Dockerfile ."
+    VALUE1="container-${i}"
+    $(echo $IN | tr ";" "\n")
+    echo ${VALUE2}
+    echo ${VALUE1}
+    docker build -t ${VALUE1} -f ${VALUE2} ;
+    #docker images
+done
+
+
+for i in $(ls -d */); 
+    do echo ${i}
+    VALUE2="${i}/Dockerfile ."
+    VALUE1="container-${i}"
+    $(echo $IN | tr ";" "\n")
+    echo ${VALUE2}
+    echo ${VALUE1}
+    #docker build -t ${VALUE1} -f ${VALUE2} ;
+    #docker images
+done
